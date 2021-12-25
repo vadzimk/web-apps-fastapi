427625b88acda609036e26a1c68b6f903d39e2f8
vadzim <vadzimk@hotmail.com>
2021-12-25 12:23:54 -0800

    render basic chameleon template

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
+import fastapi.responses
 from fastapi import FastAPI
+from fastapi_chameleon import template, global_init
 import uvicorn
 
+global_init('templates')
 app = FastAPI()
 
+
 @app.get('/')
-def index():
-    return "Hello world"
+@template(template_file='index.html')
+def index(user: str = 'anon'):
+
+    return {
+        'user_name': user
+    }
 
 if __name__ == '__main__':
-    uvicorn.run(app)
+    uvicorn.run  (app)
